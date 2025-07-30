da7bfa2404cf1be69bd4871394d6e76cc596b016
GOGOKETSU <319shidou@gmail.com>
2025-07-31 01:18:20 +0900

    微調整

--- app/page.tsx
+++ app/page.tsx
@@ -1,31 +1,6 @@
 // app/page.tsx
 import Header from "./components/Header";
 import NewsList from "./components/NewsList";
-import type { Metadata } from "next";
-
-export const metadata: Metadata = {
-  title: "Ani news by AI",
-  description: "Aniニュースは、世界のニュースをAIにより中立・公平にまとめて配信しています。",
-  openGraph: {
-    title: "Ani news by AI",
-    description: "Aniニュースは、世界のニュースをAIにより中立・公平にまとめて配信しています。",
-    url: "https://ai-and-peace.com",
-    siteName: "Ani news by AI",
-    images: [
-      {
-        url: "/image/ogp.png",
-        width: 1200,
-        height: 630,
-      },
-    ],
-    type: "website",
-  },
-  twitter: {
-    card: "summary_large_image",
-    site: "@YourTwitterHandle",
-  },
-};
-
 
 export default function Home() {
   return (
